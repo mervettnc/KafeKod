674f5c6acbb6be7368d834311bc8ebaab562921d
mervettnc <mrvttnc96@gmail.com>
2020-02-17 21:26:54 +0300

    son değişiklikler

---
+++ KafeKod.Data/KafeVeri.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KafeKod.Data
+{
+    public class KafeVeri
+    {
+        public string Urunler { get; set; }
+        public string AktifSiparisler { get; set; }
+        public string GecmisSiparisler { get; set; }
+       
+    }
+}
